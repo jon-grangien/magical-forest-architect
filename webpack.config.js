0e52c6182586ecd354da34467a83b126c7774e53
codingInSpace <jonathan.grangien@gmail.com>
2016-12-08 12:52:31 +0100

    Init project structure with webpack and es6

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	devtool: 'source-map',
+	entry: [
+		'webpack-hot-middleware/client',
+		'babel-polyfill',
+    './src/main.js'
+  ],
+  output: {
+    path: './public/',
+    filename: 'bundle.js'
+  },
+	plugins: [
+		//new webpack.optimize.UglifyJsPlugin({minimize: true}),
+		new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: { presets: [ 'es2015', 'stage-0' ] }
+			},
+			{
+				test: /(\.scss|\.css)$/,
+				loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]-[local]___[hash:base64:5]!sass-loader?sourceMap'
+			}
+		]
+  }
+};

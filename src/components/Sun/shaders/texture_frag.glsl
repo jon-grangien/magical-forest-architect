60dcd43d3f800eacadb661f488f95defba9336ac
codingInSpace <jonathan.grangien@gmail.com>
2017-11-01 11:14:13 +0100

    Change sun shader to use voronoi

--- src/components/Sun/shaders/texture_frag.glsl
+++ src/components/Sun/shaders/texture_frag.glsl
@@ -1,17 +1,149 @@
 varying vec2 vUv;
-varying float noise;
-uniform sampler2D u_sunTexture;
+uniform float u_time;
 
-float random( vec3 scale, float seed ){
-  return fract( sin( dot( gl_FragCoord.xyz + seed, scale ) ) * 43758.5453 + seed ) ;
+// Cellular noise ("Worley noise") in 2D in GLSL.
+// Copyright (c) Stefan Gustavson 2011-04-19. All rights reserved.
+// This code is released under the conditions of the MIT license.
+// See LICENSE file for details.
+// https://github.com/stegu/webgl-noise
+
+// Modulo 289 without a division (only multiplications)
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+// Modulo 7 without a division
+vec4 mod7(vec4 x) {
+  return x - floor(x * (1.0 / 7.0)) * 7.0;
+}
+
+// Permutation polynomial: (34x^2 + x) mod 289
+vec4 permute(vec4 x) {
+  return mod289((34.0 * x + 1.0) * x);
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+// Cellular noise, returning F1 and F2 in a vec2.
+// Speeded up by using 2x2 search window instead of 3x3,
+// at the expense of some strong pattern artifacts.
+// F2 is often wrong and has sharp discontinuities.
+// If you need a smooth F2, use the slower 3x3 version.
+// F1 is sometimes wrong, too, but OK for most purposes.
+vec2 cellular2x2(vec2 P) {
+#define K 0.142857142857 // 1/7
+#define K2 0.0714285714285 // K/2
+#define jitter 0.8 // jitter 1.0 makes F1 wrong more often
+	vec2 Pi = mod289(floor(P));
+ 	vec2 Pf = fract(P);
+	vec4 Pfx = Pf.x + vec4(-0.5, -1.5, -0.5, -1.5);
+	vec4 Pfy = Pf.y + vec4(-0.5, -0.5, -1.5, -1.5);
+	vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
+	p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
+	vec4 ox = mod7(p)*K+K2;
+	vec4 oy = mod7(floor(p*K))*K+K2;
+	vec4 dx = Pfx + jitter*ox;
+	vec4 dy = Pfy + jitter*oy;
+	vec4 d = dx * dx + dy * dy; // d11, d12, d21 and d22, squared
+	// Sort out the two smallest distances
+#if 0
+	// Cheat and pick only F1
+	d.xy = min(d.xy, d.zw);
+	d.x = min(d.x, d.y);
+	return vec2(sqrt(d.x)); // F1 duplicated, F2 not computed
+#else
+	// Do it right and find both F1 and F2
+	d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap if smaller
+	d.xz = (d.x < d.z) ? d.xz : d.zx;
+	d.xw = (d.x < d.w) ? d.xw : d.wx;
+	d.y = min(d.y, d.z);
+	d.y = min(d.y, d.w);
+	return sqrt(d.xy);
+#endif
+}
+
+//
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
+float snoise(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+vec3 c_white = vec3(1.0, 1.0, 1.0);
+vec3 c_cyan = vec3(0.37, 1.0, 1.0) * 0.8;
+float freq = 10.0;
+
 void main() {
-  // get a random offset
-  float r = .01 * random( vec3( 12.9898, 78.233, 151.7182 ), 0.0 );
-  // lookup vertically in the texture, using noise and offset
-  // to get the right RGB colour
-  vec2 tPos = vec2( 0, 1.0 - 1.3 * noise + r );
-  vec4 color = texture2D( u_sunTexture, tPos );
-  gl_FragColor = vec4( color.rgb, 1.0 );
+    float sd = vUv.x + 0.05 * snoise(vec2(freq*vUv.x, freq*vUv.y));
+    float td = vUv.y + 0.05 * snoise(vec2(freq*vUv.x, freq*vUv.y));
+
+    vec2 f1f2 = cellular2x2(vec2(sd * sin(0.1 * u_time), td * cos(0.2 * u_time)));
+    f1f2 = cellular2x2(vec2(snoise(vec2(f1f2.x, f1f2.y))));
+    f1f2 = cellular2x2(vec2(snoise(vec2(f1f2.x, f1f2.y))));
+
+    float noise = clamp(2.0 * (f1f2.y - f1f2.x), 0.0, 1.0);
+    vec3 out_color = mix(c_white, c_cyan, smoothstep(0.0, 0.9, noise));
+    gl_FragColor = vec4(out_color, 1.0);
 }
